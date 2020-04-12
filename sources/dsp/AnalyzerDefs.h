b63d134863bc06bb8a60120bb96e55c228120013
Jean Pierre Cimalando <jp-dev@inbox.ru>
2020-04-12 18:46:18 +0200

    Limit the fft size maximum to 16k

--- sources/dsp/AnalyzerDefs.h
+++ sources/dsp/AnalyzerDefs.h
@@ -2,7 +2,7 @@
 #include <cstdint>
 
 static constexpr uint32_t kStftMinSizeLog2 = 8;
-static constexpr uint32_t kStftMaxSizeLog2 = 16;
+static constexpr uint32_t kStftMaxSizeLog2 = 14;
 static constexpr uint32_t kStftDefaultSizeLog2 = 10;
 
 static constexpr uint32_t kStftNumSizes = 1 + kStftMaxSizeLog2 - kStftMinSizeLog2;
