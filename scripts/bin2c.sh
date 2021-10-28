931c2447805ae553b0284e89fea4e087bfbedab3
Jean Pierre Cimalando <jp-dev@inbox.ru>
2021-10-28 13:25:45 +0200

    Build with cmake

---
+++ scripts/bin2c.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+convert2c() {
+    od -An -tx1 -v | awk '{for(i=1;i<=NF;i++){printf "0x%s,",$i}}'
+}
+
+case "$#" in
+    0) convert2c ;;
+    1) cat "$1" | convert2c ;;
+    *) exit 1 ;;
+esac
